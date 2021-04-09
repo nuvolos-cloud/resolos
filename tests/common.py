0c61be365d2a1d3406e887651d9ae34cac2e80df
Tamas Simon <tamas.simon@alphacruncher.com>
2021-04-09 07:49:44 +0200

    Better error message processing in tests

---
+++ tests/common.py
@@ -0,0 +1,15 @@
+import logging
+from click.testing import Result
+
+logger = logging.getLogger(__name__)
+
+
+def verify_result(result: Result):
+    if result.exit_code != 0:
+        logger.info(result.output)
+        if result.exception:
+            raise result.exception
+        else:
+            raise Exception(
+                f"Exit code was {result.exit_code}, but no exception was caught"
+            )
--- tests/test_info.py
+++ tests/test_info.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from click.testing import CliRunner
 from resolos.interface import res_info
+from tests.common import verify_result
 import logging
 
 logger = logging.getLogger(__name__)
@@ -9,5 +10,4 @@ logger = logging.getLogger(__name__)
 def test_info():
     runner = CliRunner()
     with runner.isolated_filesystem() as fs:
-        result = runner.invoke(res_info)
-        assert result.exit_code == 0, result.stderr
+        verify_result(runner.invoke(res_info))
