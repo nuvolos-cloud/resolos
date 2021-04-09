3b55b9a3e8a940562c5de9c399359c02c2b64b63
Tamas Simon <tamas.simon@alphacruncher.com>
2021-04-09 05:55:35 +0200

    Added more test skeletons with logging

---
+++ tests/test_check.py
@@ -0,0 +1,12 @@
+from click.testing import CliRunner
+from resolos.interface import res_check
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def test_check():
+    runner = CliRunner()
+    with runner.isolated_filesystem() as fs:
+        result = runner.invoke(res_check)
+        assert result.exit_code == 0, result.output
---
+++ tests/test_init.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from click.testing import CliRunner
+from resolos.interface import res_init, res_run
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def test_init_empty():
+    runner = CliRunner()
+    with runner.isolated_filesystem() as fs:
+        logger.info("Creating new project")
+        result = runner.invoke(res_init, ["-y"])
+        assert result.exit_code == 0, result.output
+        logger.info("Checking conda version")
+        result2 = runner.invoke(res_run, ["conda --version"])
+        assert result2.exit_code == 0, result.output
+
+
+def test_download():
+    runner = CliRunner()
+    with runner.isolated_filesystem() as fs:
+        logger.info("Checking conda version")
+        result = runner.invoke(
+            res_init,
+            [
+                "-s",
+                "https://resolos.s3.eu-central-1.amazonaws.com/examples/data_with_pandas.tar.gz",
+            ],
+        )
+        assert result.exit_code == 0, result.output
+        assert (Path(fs) / "process_dataset.py").exists(), result.output
